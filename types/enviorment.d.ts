1efde3959f8a04a4301968515b37b16490f47408
Shivansh-Khunger <khunger.shivansh@outlook.com>
2024-08-30 11:35:17 +0530

    chore: add types for token and cookie env's

--- types/enviorment.d.ts
+++ types/enviorment.d.ts
@@ -1,8 +1,10 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            NODE_ENV: 'development' | 'production';
+            NODE_ENV: "development" | "production";
             DATABSE_URI: string;
+            JWT_ACCESS_SECRET_KEY: string;
+            JWT_REFRESH_SECRET_KEY: string;
         }
     }
 }
